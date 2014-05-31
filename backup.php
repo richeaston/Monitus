87f4fd4aef280d6523bbaea35b43498a9f5d8f80
richard easton <rich.easton@gmail.com>
2014-05-31 09:33:17 +0100

    backup entry in alert log

--- backup.php
+++ backup.php
@@ -5,6 +5,10 @@
 		for ($i=0; $i<count($files); $i++) {
 			copy($files[$i], "backups/" . $files[$i]);
 		}
+	$logfile = "log.csv";
+	$date = date('d-m-Y H:i:s');
+	$content = file_get_contents($logfile);
+	file_put_contents($logfile, $date  . ",Backup Successful,Settings and assets backed up\n" . $content, LOCK_EX);
 	header( 'Location: settings.php' ) ;
 				
 		?>
